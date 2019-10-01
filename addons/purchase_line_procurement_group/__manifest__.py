49325d218328bcb6cfe237355cdba99d1f8d4ff1
dennyard <sematics.info@gmail.com>
2019-10-01 23:09:03 +0700

    update lagi
    update

---
+++ addons/purchase_line_procurement_group/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2018 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Purchase Line Procurement Group",
+    "summary": "Group purchase order line according to procurement group",
+    "version": "1.0.1.1.0",
+    "category": "Purchase",
+    "website": "https://gitlab.com/flectra-community/purchase-workflow",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": [
+        "purchase",
+    ],
+    "data": [
+        "views/purchase.xml",
+    ],
+}
